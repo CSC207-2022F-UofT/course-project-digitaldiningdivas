479e645f39f51f6fc56200164f12ae770e8dcd4a
mitchellpotter99 <mitch.potter@mail.utoronto.ca>
2022-12-08 20:11:57 -0500

    Added comments

--- src/main/java/Interface_and_Adapters/DishMenuScreens/DishScreen.java
+++ src/main/java/Interface_and_Adapters/DishMenuScreens/DishScreen.java
@@ -8,19 +8,22 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 import java.util.List;
 
+/**
+ * The dish screen class that formats and displays dish information to the UI.
+ */
 public class DishScreen extends JPanel implements ActionListener {
 
     DishController dishController;
 
-    public DishScreen(DishController dishController, String account) throws IOException {
+    /**
+     * The constructor for the DishScreen when activated.
+     * @param dishController: The given controller for the instantiation of the DishScreen.
+     */
+    public DishScreen(DishController dishController, String username) {
         CardLayout cards = new CardLayout();
         this.setLayout(cards);
-
-        JPanel outerPanel = new JPanel();
-        outerPanel.setLayout(new CardLayout());
 
         JPanel subPanel = new JPanel(); //panel for button grid
 
@@ -33,10 +36,10 @@ public class DishScreen extends JPanel implements ActionListener {
         this.dishController = dishController;
 
         DishDataAccess dishes;
-        dishes = new DishFileReader("src/main/java/Frameworks_and_Drivers/Dishes.csv");
+        dishes = new DishFileReader("Dishes.csv");
 
-        for(String key: dishes.getDish("src/main/java/Frameworks_and_Drivers/Dishes.csv").keySet()){
-            List<List<String>> dishList = dishes.getDish("src/main/java/Frameworks_and_Drivers/Dishes." +
+        for(String key: dishes.getDish("Dishes.csv").keySet()){
+            List<List<String>> dishList = dishes.getDish("Dishes." +
                     "csv").get(key);
             for (List<String> element : dishList) {
                 JButton button = new JButton(element.get(0) + " (" + key + ")");
@@ -49,7 +52,7 @@ public class DishScreen extends JPanel implements ActionListener {
                     //opens restaurant window with jbuttons from "home" screen
                     {
                         DishPopUp popUp = new DishPopUp(element.get(0), element.get(1), element.get(2), element.get(3),
-                                element.get(4), DishScreen.this, account);
+                                element.get(4));
                         DishScreen.this.add(popUp, "card1");
                         GridBagConstraints c = new GridBagConstraints();
                         JButton backButton = new JButton("Back");
@@ -76,6 +79,11 @@ public class DishScreen extends JPanel implements ActionListener {
 
 
     }
+
+    /**
+     * A method for when an action is performed within the menu.
+     * @param evt the event to be processed
+     */
     public void actionPerformed(ActionEvent evt) {
         System.out.println("Click " + evt.getActionCommand());
     }
