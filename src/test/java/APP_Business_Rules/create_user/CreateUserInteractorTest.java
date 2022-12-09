d8746d6c07a55983d15f807b31f5f5d61a860081
Moe-Reda <108328374+Moe-Reda@users.noreply.github.com>
2022-12-08 20:10:57 -0500

    create user use case test

--- src/test/java/APP_Business_Rules/create_user/CreateUserInteractorTest.java
+++ src/test/java/APP_Business_Rules/create_user/CreateUserInteractorTest.java
@@ -10,24 +10,33 @@ import Interface_and_Adapters.start_up_screens.CreateUserResponse;
 import org.junit.jupiter.api.Test;
 
 import java.util.Objects;
+import java.util.Random;
 
 
 class CreateUserInteractorTest {
 
     // seems impossible to create a functional test.
     // The use case is tested more practically by running the application.
+    Random random = new Random();
     CreateUserGateway user = new UserFile("./users.csv");
     CreateUserPresenter presenter = new CreateUserResponse();
     UserFactory userFactory = new AccountFactory();
     CreateUserInputBoundary interactor = new CreateUserInteractor(
             user, userFactory, presenter);
     CreateUserController controller = new CreateUserController(interactor);
+    String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "x", "y", "z"};
 
 
     @Test
     void createNewUser() {
-        CreateUserResponseModel res = controller.create("John", "password", "password");
-        assert user.findAccountUser("John");
-        assert Objects.equals(res.getLogin(), "John");
+        String name = "";
+        String password = "";
+        while(user.findAccountUser("John")&&name.equals("")){
+            name = alphabet[random.nextInt(26)] + alphabet[random.nextInt(26)] + alphabet[random.nextInt(26)] + alphabet[random.nextInt(26)];
+            password = alphabet[random.nextInt(26)] + alphabet[random.nextInt(26)] + alphabet[random.nextInt(26)] + alphabet[random.nextInt(26)] + alphabet[random.nextInt(26)] + alphabet[random.nextInt(26)] + alphabet[random.nextInt(26)] + alphabet[random.nextInt(26)];
+        }
+        CreateUserResponseModel res = controller.create(name, password, password);
+        assert user.findAccountUser(name);
+        assert Objects.equals(res.getLogin(), name);
     }
 }
